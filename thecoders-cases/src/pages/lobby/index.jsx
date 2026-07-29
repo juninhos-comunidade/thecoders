c3a9183212856f908edc8ace1cfa7d39db82317c
Laís Brum <laisbrme@gmail.com>
2026-07-29 00:16:08 -0300

    ✨ feat(lobby): integra o componente de navegação à página lobby

---
+++ thecoders-cases/src/pages/lobby/index.jsx
@@ -0,0 +1,14 @@
+import "./index.css";
+import Navbar from "../../components/navbar";
+
+import { useNavigate } from "react-router-dom";
+
+export default function Lobby() {
+    // const navigate = useNavigate();
+
+    return (
+        <>
+			<Navbar />
+        </>
+    );
+}
