bf82677910d0ae17ac52481bff75a8daf569a8dc
Matheus Santana <santanapbi@gmail.com>
2026-07-31 23:27:21 -0300

    ✨ feat(router): adiciona rota de cadastro e esqueci a senha

--- thecoders-cases/src/router.jsx
+++ thecoders-cases/src/router.jsx
@@ -1,4 +1,6 @@
 import Login from "./pages/login";
+import EsqueciSenha from "./pages/esqueci-senha";
+import CadastroPage from "./pages/cadastro"; // ajuste o caminho conforme onde você salvou o arquivo
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -7,6 +9,8 @@ export default function Router() {
 		<BrowserRouter>
 			<Routes>
 				<Route path="/" element={<Login />} />
+				<Route path="/recuperar" element={<EsqueciSenha />} />
+				<Route path="/cadastro" element={<CadastroPage />} />
 				{/* <Route path="/lobby" element={<Lobby />} /> */}
 			</Routes>
 		</BrowserRouter>
