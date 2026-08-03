56868e22d86db61ce97860f7b6cb6a4e5b0f6861
Laís Brum <laisbrme@gmail.com>
2026-08-03 00:47:54 -0300

    :bug: fix(buttons): remove verificação de rota que impedia a renderização do botão

--- thecoders-cases/src/components/buttons/index.jsx
+++ thecoders-cases/src/components/buttons/index.jsx
@@ -1,32 +1,27 @@
 import "./index.css";
 import "../../App.css";
-import { useLocation, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export default function Buttons({ label, variant = "primary", disabled = false, page }) {
     const className = ["btn-primary", variant === "white" ? "btn-primary--white" : ""].join(" ").trim();
-    const location = useLocation();
     const navigate = useNavigate();
 
-    if (location.pathname !== "/last-result" && location.pathname !== "/on-case") {
-        return (
-            <button
-                type="button"
-                className={className}
-                disabled={disabled}
-                onClick={() => {
-                    if (!disabled && page) {
-                        navigate(page);
-                    }
-                }}
-            >
-                <div className="content">
-                    <div className="state-layer">
-                        <div className="button-text">{label}</div>
-                    </div>
+    return (
+        <button
+            type="button"
+            className={className}
+            disabled={disabled}
+            onClick={() => {
+                if (!disabled && page) {
+                    navigate(page);
+                }
+            }}
+        >
+            <div className="content">
+                <div className="state-layer">
+                    <div className="button-text">{label}</div>
                 </div>
-            </button>
-        );
-    }
-
-    return null;
+            </div>
+        </button>
+    );
 }
