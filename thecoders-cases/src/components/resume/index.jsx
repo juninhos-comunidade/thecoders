8e6a0c02be65683cb325720cd7a8ba83f92f8ac2
Laís Brum <laisbrme@gmail.com>
2026-08-03 02:14:03 -0300

    🎨 style(resume): ajusta os estilos do componente de resume

--- thecoders-cases/src/components/resume/index.jsx
+++ thecoders-cases/src/components/resume/index.jsx
@@ -1,11 +1,10 @@
 import "./index.css";
-import "../../App.css";
 
 export default function Resume({ texto }) {
     return (
         <div className="container-resume">
             <h3>Resumo</h3>
-            <p className="text" dangerouslySetInnerHTML={{ __html: texto }} />
+            <p className="resume-text" dangerouslySetInnerHTML={{ __html: texto }} />
         </div>
     );
 }
