23add114dd3c0406e2983b04f3b6c63ba3d7d286
Laís Brum <laisbrme@gmail.com>
2026-08-06 01:03:47 -0300

    🔗 refactor(api): registra router de avaliacao no main.py

--- thecoders-cases-back/main.py
+++ thecoders-cases-back/main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
 
 from database.supabase_client import supabase
-from routers import solucao
+from routers import solucao, avaliacao
 
 app = FastAPI()
 
 app.include_router(solucao.router)
+app.include_router(avaliacao.router)
 
 
 @app.get("/health")
