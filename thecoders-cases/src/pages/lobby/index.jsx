672386c47ec38c14d38f4c4ce312e574c3a7ffc4
Laís Brum <laisbrme@gmail.com>
2026-08-10 22:27:50 -0300

    ✨ feat(lobby): utiliza dados do usuário na página e no card de cases

--- thecoders-cases/src/pages/lobby/index.jsx
+++ thecoders-cases/src/pages/lobby/index.jsx
@@ -1,11 +1,14 @@
 import "./index.css";
 import "../../App.css";
+import { useLocation } from "react-router-dom";
 import Navbar from "../../components/navbar";
 import CardProfile from "../../components/card-profile";
 import CardCases from "../../components/card-cases";
 
 export default function Lobby() {
-    const user = "Lorena";
+    const location = useLocation();
+    const usuarioId = location.state?.usuarioId;
+    const usuarioNome = location.state?.usuarioNome || "Usuário";
 
     return (
         <>
@@ -16,13 +19,19 @@ export default function Lobby() {
 
             <div className="container-lobby">
                 <div className="textos">
-                    <h3>Bem-vindo, {user}!</h3>
+                    <h3>Bem-vindo, {usuarioNome}!</h3>
                     <div className="title">Continue evoluindo resolvendo desafios reais</div>
                 </div>
 
                 <div className="cards">
                     <CardProfile nivel="Estagiário" exp="0" nextLevel="Júnior" />
-                    <CardCases num="0" dificult="🟢 Fácil" limit="10" />
+                    <CardCases
+                        num="0"
+                        dificult="🟢 Fácil"
+                        limit="10"
+                        usuarioId={usuarioId}
+                        usuarioNome={usuarioNome}
+                    />
                 </div>
             </div>
         </>
