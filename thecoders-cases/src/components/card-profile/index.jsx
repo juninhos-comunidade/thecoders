347023cd64e90fce7d1d05295076abcb9434a75a
Laís Brum <laisbrme@gmail.com>
2026-08-12 02:44:40 -0300

    🧹 chore(card-profile): ajusta o limite de experiência

--- thecoders-cases/src/components/card-profile/index.jsx
+++ thecoders-cases/src/components/card-profile/index.jsx
@@ -7,9 +7,9 @@ export default function CardProfile({ nivel, exp, nextLevel }) {
             <h3>Seu Perfil</h3>
             <p className="text">{nivel}</p>
             <div className="infos">
-                <p className="exp">XP: {exp}/1000</p>
+                <p className="exp">XP: {exp}/100</p>
                 <div className="progress-bar">
-                    <div className="progress" style={{ width: `${(exp / 1000) * 100}%` }}></div>
+                    <div className="progress" style={{ width: `${(exp / 100) * 100}%` }}></div>
                 </div>
                 <div className="box">
                     <p className="next-level">Próximo nível: </p>
